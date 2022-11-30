166ebcce59e4dc03242035746ecbd723757033d4
Duke Ahn <lapduke@dukes-mbp.lan>
2022-11-29 21:23:06 -0800

    create reset button

--- jscript.js
+++ jscript.js
@@ -1,4 +1,6 @@
 const cellArray = [];
+const button = document.getElementById('reset')
+let mouseStatus = ""
 
 function createGrid (){
     for (let i = 0; i < 256; i++){
@@ -6,22 +8,45 @@ function createGrid (){
         const grid = document.getElementById('container');
         newDiv.innerHTML = "";
         newDiv.classList.add(`cell${i}`)
-        newDiv.classList.add('cell')
         cellArray.push(`.cell${i}`);
         grid.appendChild(newDiv);
     }
 }
 createGrid();
 
+button.addEventListener("click", resetButton)
+addEventListener("mousedown", mouseDown)
+addEventListener("click", mouseClick)
+addEventListener("mouseup", mouseRelease)
+
 cellArray.forEach(trigger)
 
+
 function trigger(){
-    addEventListener("mouseover", turnBlackTwo)
-    
+    addEventListener("mouseover", turnBlack)
 }
 
-function turnBlackTwo(){
+function turnBlack(){
     let e = event.target
-    if(e == '[object HTMLDivElement]')
+    if(e == '[object HTMLDivElement]' && mouseStatus == 'on')
     e.classList.add('black')
+}
+
+function mouseDown(){
+    return mouseStatus = 'on'
+}
+
+function mouseClick(){
+    return mouseStatus = 'click'
+}
+
+function mouseRelease(){
+    return mouseStatus = 'off'
+}
+
+function resetButton (){
+    for (let i = 0; i < 256; i++){
+        const c = document.querySelector(`.cell${i}`)
+        c.classList.remove('black')
+    }
 }
