938e470a49e72b7568a406ecb1a944a24e1a3fb0
Duke Ahn <lapduke@dukes-mbp.lan>
2022-11-29 19:57:02 -0800

    create turn black function

--- jscript.js
+++ jscript.js
@@ -4,18 +4,24 @@ function createGrid (){
     for (let i = 0; i < 256; i++){
         const newDiv = document.createElement('div');
         const grid = document.getElementById('container');
-        newDiv.innerHTML = "s";
+        newDiv.innerHTML = "";
         newDiv.classList.add(`cell${i}`)
-        cellArray.push(`.cell${i}`)
-        grid.appendChild(newDiv)
+        newDiv.classList.add('cell')
+        cellArray.push(`.cell${i}`);
+        grid.appendChild(newDiv);
     }
 }
 createGrid();
 
-for(let i = 0; i < cellArray.length; i++) {
-    cellArray[i].addEventListener("mouseover", turnBlack);
+cellArray.forEach(trigger)
+
+function trigger(){
+    addEventListener("mouseover", turnBlackTwo)
+    
 }
 
-function turnBlack (){
-    console.log("huh")
+function turnBlackTwo(){
+    let e = event.target
+    if(e == '[object HTMLDivElement]')
+    e.classList.add('black')
 }
